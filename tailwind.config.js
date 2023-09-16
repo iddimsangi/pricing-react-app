02649291fcd856731b210c9472c79b6f05080920
iddimsangi <adammsangi@gmail.com>
2023-09-16 15:55:20 +0300

    react project setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        VeryLightGrayishBlue: "hsl(240, 78%, 98%)",
+        LightGrayishBlue: "hsl(234, 14%, 74%)",
+        GrayishBlue: "hsl(233, 13%, 49%)",
+        DarkGrayishBlue: "hsl(232, 13%, 33%)",
+      },
+    },
+  },
+  plugins: [],
+};
